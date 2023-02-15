2e66134ce63852149b79861120c13e63710d6c69
Anastassia Sommer <anastassia.sommer@gmail.com>
2023-02-15 17:51:02 +0100

    api call start

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
+const latestPostsContainer = document.querySelectorAll(".latest-container");
 
 hamburger.addEventListener("click", mobileMenu);
 
@@ -18,3 +19,19 @@ function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }
+
+// Make a call to API to get list of posts
+
+async function getPosts() {
+  const url = `https://anasommer.com/api/wp-json/wp/v2/posts`;
+  const response = await fetch(url);
+  const postData = await response.json();
+
+  postData.map((post, index) => {
+    return console.log(post, index);
+  });
+}
+
+getPosts();
+
+// make function that calls getPosts() and has evenlisteners for buttons and renders posts to the page
